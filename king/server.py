b1b04e35e86da69076dbc3e807440127c106ea44
ahirreddy <ahirreddy@gmail.com>
2013-03-14 01:34:24 -0700

    It's workinggit diffgit diffgit diffclear

--- king/server.py
+++ king/server.py
@@ -1,11 +1,23 @@
 from twisted.internet import reactor
 from twisted.names import dns, client, server
+from io import BytesIO
 
 class DNSServerFactory(server.DNSServerFactory):
 	def handleQuery(self, message, protocol, address):
-		print message, protocol, address
+		query = message.queries[0]
 
-factory = DNSServerFactory()
+		x = dns.RRHeader(name=query.name.name, type=dns.NS, cls=dns.IN, ttl=0, auth=False,
+						 payload=dns.Record_NS(name='nbapuns.com', ttl=0))
+
+		ans = [x]
+		auth = []
+		add = []
+		args = (self, (ans, auth, add), protocol, message, address)
+
+		return server.DNSServerFactory.gotResolverResponse(*args)
+
+resolver = client.Resolver(servers=[('4.2.2.2', 53)])
+factory = DNSServerFactory(clients=[resolver])
 protocol = dns.DNSDatagramProtocol(factory)
 
 reactor.listenUDP(53, protocol)
