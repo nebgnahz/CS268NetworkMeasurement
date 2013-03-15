8ae18f4988108968220eb6400f34d492b240e0de
ahirreddy <ahirreddy@gmail.com>
2013-03-14 21:00:58 -0700

    changed prints

--- king/scraper.py
+++ king/scraper.py
@@ -5,41 +5,41 @@ from twisted.names import client, dns
 conn = sqlite3.connect('dns.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE dns (name varchar(255), ip int)''')
-
+c.execute('''CREATE TABLE dns (name TEXT, ip INTEGER)''')
 
 sem = defer.DeferredSemaphore(112)
 
-def ip2int(addr):                                                               
-    return struct.unpack("!I", socket.inet_aton(addr))[0]                       
+def ip2int(addr):                       
+    return struct.unpack("!I", socket.inet_aton(addr))[0]                 
 
 def int2ip(addr):                                                               
-    return socket.inet_ntoa(struct.pack("!I", addr))   
+    return socket.inet_ntoa(struct.pack("!I", addr))
 
 def got_ptr(args, addr, level):
 	(ans, auth, add) = args
 	if level == 4:
 		records = {}
-		print '\n', addr
-		print 'Additional:'
+		#print '\n', addr
+		#print 'Additional:'
 		for A in add:
 			if A.type is dns.A:
-				print A.name, A.payload.dottedQuad()
+				#print A.name, A.payload.dottedQuad()
 				records[A.name.name] = A.payload.dottedQuad()
-		print 'Authoritative:'
+		#print 'Authoritative:'
 		for NS in auth:
 			if NS.type is dns.NS:
 				if NS.payload.name.name not in records:
 					records[NS.payload.name.name] = None
-					print NS.payload.name.name, "No IP"
+					#print NS.payload.name.name, "No IP"
 		for name, ip in records.items():
 			if ip:
-				t = name, ip, ip2int(ip), int2ip(ip2int(ip))
+				#print "INSERT INTO dns VALUES ('%s',%i)" % (name, ip2int(ip))
+				c.execute("INSERT INTO dns VALUES ('%s',%i)" % (name, ip2int(ip)))
 			else:
-				t = name,
-			print t
-			#c.execute("INSERT INTO dns VALUES (?,?)", t)
-			#c.commit()
+				#print "INSERT INTO dns VALUES ('%s', NULL)" % name
+				c.execute("INSERT INTO dns VALUES ('%s', NULL)" % name)
+			conn.commit()
+			print 'Commit %s', ip
 	else:
 		lookup(postfix=addr, level=level + 1)
 
