a7f86299df52d16f5cb548610f7cd26ac0096ea4
ahirreddy <ahirreddy@gmail.com>
2013-03-14 19:07:45 -0700

    Semaphore

--- king/scraper.py
+++ king/scraper.py
@@ -1,20 +1,28 @@
 from twisted.internet import reactor, defer
 from twisted.names import client
 
-def got_ptr(*args):
-	print "Good", args
+semaphore = defer.DeferredSemaphore(5)
+
+def got_ptr(args, addr, level):
+	semaphore.release()
+	if level == 2:
+		print addr, args
+	else:
+		lookup(postfix=addr, level=level + 1)
 
 def no_ptr(*args):
-	print "Bad", args
+	semaphore.release()
+	pass
 
-def lookup(postfix=None):
+def lookup(postfix=None, level=1):
 	for octet in range(0,256):
 		if postfix:
 			addr = "%i.%s" % (octet, postfix)
 		else:
 			addr = "%i.in-addr.arpa" % octet
 
-		d = client.lookupPointer(addr).addCallback(got_ptr,addr).addErrback(no_ptr, addr)
+		semaphore.acquire()
+		client.lookupPointer(addr).addCallback(got_ptr, addr, level).addErrback(no_ptr)
 
 lookup()
 reactor.run()
