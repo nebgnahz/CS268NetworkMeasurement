c4b8bdb658d942408dde9a0cc3884923168302f7
ahirreddy <ahirreddy@gmail.com>
2013-03-17 14:11:06 -0700

    scraper 2 update

--- king/scraper2.py
+++ king/scraper2.py
@@ -1,34 +1,62 @@
-from twisted.internet import reactor, threads
+import psycopg2, socket, struct
+from twisted.internet import reactor
 from twisted.names import client, dns
 
-concurrent = 2000
+concurrent = 500
 reactor.suggestThreadPoolSize(concurrent)
 
+conn = psycopg2.connect('dbname=dns user=ahirreddy host=localhost')
+c = conn.cursor()
+
 def processResponse(args, addr, level):
-	if level == 4:
-		print "Response", args, addr
-	else:
-		lookup(postfix=addr, level=level+1)
+    if level == 4:
+        processRecords(*args)
+    else:
+        lookup(postfix=addr, level=level+1)
 
 def processError(*args):
-	pass
+    pass
+
+def processRecords(ans, auth, add):
+    records = {}
+    for A in filter(lambda x: x.type is dns.A, add):
+        records[A.name.name] = A.payload.dottedQuad()
+    for NS in filter(lambda x: x.type is dns.NS, auth):
+        if NS.payload.name.name not in records:
+            records[NS.payload.name.name] = None
+    try:
+        insertDB(records)
+    except e:
+        print e
+
+def insertDB(records):
+    for name, ip in records.items():
+        if ip:
+            query = '''INSERT INTO dns (name, ip) SELECT '%s', %i WHERE NOT EXISTS (SELECT 1 FROM dns WHERE name = '%s' and ip IS NOT NULL);''' % (name, ip2int(ip), name)
+        else:
+            query = '''INSERT INTO dns (name, ip) SELECT '%s', NULL WHERE NOT EXISTS (SELECT 1 FROM dns WHERE name = '%s');''' % (name, name)
+        c.execute(query)
+    conn.commit()
+
+def ip2int(addr):
+    return struct.unpack("!I", socket.inet_aton(addr))[0]
 
 def addTask(addr, level):
-	req = client.lookupPointer(addr)
-	req.addCallback(processResponse, addr, level)
-	req.addErrback(processError, addr)
+    req = client.lookupPointer(addr)
+    req.addCallback(processResponse, addr, level)
+    req.addErrback(processError, addr)
 
 def lookup(postfix=None, level=1):
-	for octet in range(0,20):
-		if postfix:
-			addr = "%i.%s" % (octet, postfix)
-		else:
-			addr = "%i.in-addr.arpa" % octet
+    for octet in range(0,20):
+        if postfix:
+            addr = "%i.%s" % (octet, postfix)
+        else:
+            addr = "%i.in-addr.arpa" % octet
 
-		addTask(addr, level)
+        addTask(addr, level)
 
 lookup()
 try:
-	reactor.run()
+    reactor.run()
 except KeyboardInterrupt:
-	reactor.stop()
+    reactor.stop()
