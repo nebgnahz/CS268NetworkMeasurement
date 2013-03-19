0b6991d8c8946cd89ffcd33beec1e0b951b3fa02
ahirreddy <ahirreddy@gmail.com>
2013-03-19 03:22:16 -0700

    changed timeout

--- king/scraper3.py
+++ king/scraper3.py
@@ -85,7 +85,7 @@ def lookup(ip, level, arr, id):
 
     for i in range(5-level):
         try:
-            response = dns.query.udp(query, ns, timeout=1.0)
+            response = dns.query.udp(query, ns, timeout=.5)
             arr[id] = time()
             rcode = response.rcode()
             if rcode == dns.rcode.NOERROR:
