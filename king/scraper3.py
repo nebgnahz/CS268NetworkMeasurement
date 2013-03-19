0e27f60755e04d86e4ea603ab25ed40c730e4991
ahirreddy <ahirreddy@gmail.com>
2013-03-18 17:30:39 -0700

    more space efficient queue

--- king/scraper3.py
+++ king/scraper3.py
@@ -1,52 +1,63 @@
 import dns.query, dns.resolver
 from dns.exception import DNSException
-from multiprocessing import Process
-from multiprocessing import JoinableQueue as Queue
+from multiprocessing import Process, JoinableQueue
+from types import GeneratorType
 
 ip_range = 10
 concurrent = 500
-
 default = dns.resolver.get_default_resolver()
 ns = default.nameservers[0]
+q = JoinableQueue()
 
-def ip2reverse(ip):
-    ip = ip.split('.')
-    ip.reverse()
-    return '%s.in-addr.arpa' % '.'.join(ip)
+def main():
+    for i in range(concurrent):
+        t=Process(target=doWork)
+        t.daemon=True
+        t.start()
+    q.put((None, 1))
+    q.join()
+
+def doWork():
+    while True:
+        prefix, level = q.get()
+        if prefix:
+            prefix = int2ip(prefix)
+            ips = ("%s.%i" % (prefix, octet) for octet in range(0,ip_range))
+        else:
+            ips = ("%i" % octet for octet in range(0,ip_range))
+        
+        for ip in ips:
+            auth, add = lookup(ip)
+            if auth is None and add is None:
+                pass
+            else:
+                if level < 4:
+                    q.put((ip2int(ip), level+1))
+        q.task_done()
 
 def lookup(ip):
     addr = ip2reverse(ip)
     query = dns.message.make_query(addr, dns.rdatatype.PTR)
     response = dns.query.udp(query, ns)
-
+    
     rcode = response.rcode()
     if rcode == dns.rcode.NOERROR:
         return response.authority, response.additional
     else:
         return None, None
 
-def doWork():
-    while True:
-        ip, level = q.get()
-        auth, add = lookup(ip)
-        if auth is None and add is None:
-            pass
-        else:
-            print auth, add
-            if level < 4:
-                for octet in range(0,ip_range):
-                    ip = "%s.%i" % (ip, octet)
-                    q.put((ip, level+1))
-        q.task_done()
+#############
+# Utilities #
+#############
+def ip2int(addr):
+    return tuple(addr.split('.'))
 
-q=Queue(concurrent*2)
+def int2ip(addr):
+    return '.'.join(addr)
 
-for i in range(concurrent):
-    t=Process(target=doWork)
-    t.daemon=True
-    t.start()
+def ip2reverse(ip):
+    ip = ip.split('.')
+    ip.reverse()
+    return '%s.in-addr.arpa' % '.'.join(ip)
 
-
-for octet in range(0,ip_range):
-    q.put((str(octet), 1))
-q.join()
+main()
