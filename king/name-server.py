8ad5c2ab55b8c4278fe6ddd927566f7bcaf7590e
ahirreddy <ahirreddy@gmail.com>
2013-03-27 13:54:05 -0700

    wrapped code in try except

--- king/name-server.py
+++ king/name-server.py
@@ -3,26 +3,29 @@ from twisted.names import dns, client, server
 
 class DNSServerFactory(server.DNSServerFactory):
     def handleQuery(self, message, protocol, address):
-        query = message.queries[0]
-        target = query.name.name
-        origin = target.split('.')[0]
-        origin = origin.split('-')
+        try:
+            query = message.queries[0]
+            target = query.name.name
+            origin = target.split('.')[0]
+            origin = origin.split('-')
 
-        origin_ns = origin[-1]
-        origin_ns_name = "%s.nbappuns.com" % origin_ns
-        origin_ip = '.'.join(origin[:4])
+            origin_ns = origin[-1]
+            origin_ns_name = "%s.nbappuns.com" % origin_ns
+            origin_ip = '.'.join(origin[:4])
 
-        NS = dns.RRHeader(name=target, type=dns.NS, cls=dns.IN, ttl=0, auth=True,
-                         payload=dns.Record_NS(name=origin_ns_name, ttl=0))
-        A = dns.RRHeader(name=origin_ns_name, type=dns.A, cls=dns.IN, ttl=0,
-                        payload=dns.Record_A(address=origin_ip, ttl=None))
+            NS = dns.RRHeader(name=target, type=dns.NS, cls=dns.IN, ttl=0, auth=True,
+                             payload=dns.Record_NS(name=origin_ns_name, ttl=0))
+            A = dns.RRHeader(name=origin_ns_name, type=dns.A, cls=dns.IN, ttl=0,
+                            payload=dns.Record_A(address=origin_ip, ttl=None))
 
-        ans = []
-        auth = [NS]
-        add = [A]
-        args = (self, (ans, auth, add), protocol, message, address)
+            ans = []
+            auth = [NS]
+            add = [A]
+            args = (self, (ans, auth, add), protocol, message, address)
 
-        return server.DNSServerFactory.gotResolverResponse(*args)
+            return server.DNSServerFactory.gotResolverResponse(*args)
+        except Exception:
+            print "Bad Request"
 
 factory = DNSServerFactory()
 protocol = dns.DNSDatagramProtocol(factory)
