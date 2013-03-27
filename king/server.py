53733ac65a652e7dfd21d4cc2483a6a2be92c32a
ahirreddy <ahirreddy@gmail.com>
2013-03-27 12:26:17 -0700

    return ns and a record

--- king/server.py
+++ king/server.py
@@ -2,19 +2,21 @@ from twisted.internet import reactor
 from twisted.names import dns, client, server
 
 class DNSServerFactory(server.DNSServerFactory):
-	def handleQuery(self, message, protocol, address):
-		query = message.queries[0]
+    def handleQuery(self, message, protocol, address):
+        query = message.queries[0]
 
-		print query.name.name
-		x = dns.RRHeader(name=query.name.name, type=dns.NS, cls=dns.IN, ttl=0, auth=False,
-						 payload=dns.Record_NS(name='nbapuns.com', ttl=0))
+        print query.name.name
+        NS = dns.RRHeader(name=query.name.name, type=dns.NS, cls=dns.IN, ttl=0, auth=True,
+                         payload=dns.Record_NS(name='ns1.nbapuns.com', ttl=0))
+        A = dns.RRHeader(name='ns1.nbapuns.com', type=dns.A, cls=dns.IN, ttl=0,
+                        payload=dns.Record_A(address='127.0.0.1', ttl=None))
 
-		ans = [x]
-		auth = []
-		add = []
-		args = (self, (ans, auth, add), protocol, message, address)
+        ans = []
+        auth = [NS]
+        add = [A]
+        args = (self, (ans, auth, add), protocol, message, address)
 
-		return server.DNSServerFactory.gotResolverResponse(*args)
+        return server.DNSServerFactory.gotResolverResponse(*args)
 
 factory = DNSServerFactory()
 protocol = dns.DNSDatagramProtocol(factory)
