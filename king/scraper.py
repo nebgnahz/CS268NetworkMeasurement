b8e406b62051e304d7334718fd118ed3e7e094d0
ahirreddy <ahirreddy@gmail.com>
2013-03-26 17:49:20 -0700

    changed prefix len

--- king/scraper.py
+++ king/scraper.py
@@ -77,7 +77,7 @@ def doWork(arr, id, dictionary):
             continue
 
         if prefix:
-            if len(prefix) is 1:
+            if len(prefix) is 2:
                 print prefix
             prefix = tuple2ip(prefix)
             ips = ("%s.%i" % (prefix, octet) for octet in range(0,octet_range))
