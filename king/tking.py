fb067300e17b7264f807cd47cfb0e1f9315c87fc
ahirreddy <ahirreddy@gmail.com>
2013-03-28 15:59:20 -0700

    converted turbo king into rpyc service

--- king/tking.py
+++ king/tking.py
@@ -18,9 +18,69 @@ target1 = '8.8.8.8'
 target2 = 'ns1.google.com'
 target2_ip = '216.239.32.10'
 
-start_time = None
 
+###############
+# RPC Service #
+###############
+class TurboKingService(rpyc.Service):
+    def on_connect(self):
+        pass
+
+    def on_disconnect(self):
+        pass
+
+    def exposed_get_latency(self, t1, ip1, t2, ip2):
+        query_id = randrange(0, sys.maxint)
+        
+        # Setup DNS Server
+        factory = DNSServerFactory(query_id)
+        protocol = twisted_dns.DNSDatagramProtocol(factory)
+        reactor.listenUDP(53, protocol)
+        reactor.listenTCP(53, factory)
+
+        # Start DNS Client
+        t=Thread(target=DnsClient)
+        t.start()
+
+        # Start DNS Server
+        reactor.run()
+
+        if factory.start_time:
+            return t.end_time - factory.start_time
+        else:
+            return None
+
+##########
+# Client #
+##########
+class DNSClient(Thread):
+    def __init__(self, query_id, target1):
+        Thread.__init__(self)
+        self.query_id = query_id
+        self.target1 = target1
+        self.end_time = None
+
+    def run(self):
+        sleep(1) # Wait for DNS Server to Start
+        addr = "final.%i.%s" % (self.query_id, myAddr)
+        query = dns.message.make_query(addr, dns.rdatatype.A)
+        response = dns.query.udp(query, self.target1, timeout=5)
+        end_time = datetime.now()
+        reactor.callFromThread(reactor.stop)        
+        print "Recieved Response:"
+        print response
+
+
+##############
+# DNS Server #
+##############
 class DNSServerFactory(server.DNSServerFactory):
+    def __init__(self, query_id, authorities=None, caches=None, clients=None, verbose=0):
+        server.DNSServerFactory.__init__(self,authorities,caches,clients,verbose)
+        self.query_id = query_id
+        self.start_time = None
+
+    #TODO: Stop Handling of Queries after seeing a valid one
     def handleQuery(self, message, protocol, address):
         print "Recieved Query"
         print address
@@ -28,15 +88,16 @@ class DNSServerFactory(server.DNSServerFactory):
 
         global start_time, query_id
         try:
-            start_time = datetime.now()
             query = message.queries[0]
             target = query.name.name
             print target
             dummy, id, origin = target.split('.')[0:3]
 
-            if int(id) != query_id:
+            if int(id) != self.query_id:
                 print "Query ID Doesn't Match"
                 raise Exception
+            else:
+                self.start_time = datetime.now()
 
             NS = twisted_dns.RRHeader(name=target, type=twisted_dns.NS, cls=twisted_dns.IN, ttl=0, auth=True,
                              payload=twisted_dns.Record_NS(name=target2, ttl=0))
@@ -51,35 +112,3 @@ class DNSServerFactory(server.DNSServerFactory):
             return server.DNSServerFactory.gotResolverResponse(*args)
         except Exception, e:
             print "Bad Request", e
-
-query_id = randrange(0, sys.maxint)
-
-##########
-# Server #
-##########
-factory = DNSServerFactory()
-protocol = twisted_dns.DNSDatagramProtocol(factory)
-reactor.listenUDP(53, protocol)
-reactor.listenTCP(53, factory)
-
-##########
-# Client #
-##########
-def client():
-    sleep(1)
-    addr = "final.%i.%s" % (query_id, myAddr)
-    query = dns.message.make_query(addr, dns.rdatatype.A)
-    response = dns.query.udp(query, target1, timeout=5)
-    end_time = datetime.now()
-    print "Recieved Response:"
-    try:
-        print "Time: ", end_time - start_time
-    except:
-        print 'Never Recieved Server Query'
-    print response
-    #reactor.callFromThread(reactor.stop)
-
-t=Thread(target=client)
-t.start()
-
-reactor.run()
