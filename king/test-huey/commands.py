bf85e98da818f845e88935a018dbf5bb24e16c46
ahirreddy <ahirreddy@gmail.com>
2013-03-28 15:14:34 -0700

    Removed Huey

--- king/test-huey/commands.py
+++
@@ -1,9 +0,0 @@
-# commands.py
-from huey import queue_command
-
-from config import invoker # import the invoker we instantiated in config.py
-
-
-@queue_command(invoker)
-def count_beans(num):
-    print 'Counted %s beans' % num
--- king/test-huey/task.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from config import Configuration # import the configuration class
-from commands import count_beans # import our command
-
-
-if __name__ == '__main__':
-    beans = raw_input('How many beans? ')
-    count_beans(int(beans))
-    print 'Enqueued job to count %s beans' % beans
