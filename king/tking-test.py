a8d57d573c12ded8f80b7470e5dc17ba14b2b540
ahirreddy <ahirreddy@gmail.com>
2013-03-29 13:44:10 -0700

    test multiple hosts

--- king/tking-test.py
+++ king/tking-test.py
@@ -1,17 +1,27 @@
 from rpyc.utils.factory import ssh_connect
 from plumbum import SshMachine
+from termcolor import colored
 
-host = 'planetlab-1.imperial.ac.uk'
-# host = 'pl2.planetlab.ics.tut.ac.jp'
+hosts = map(string.strip,open('pl-host-list').readlines())
 
-rem = SshMachine(host, user='ucb_268_measure', keyfile='~/.ssh/id_rsa')
+for host in hosts:
+    try:
+        try:
+            conn = rpyc.connect(host, 18861)
+            print colored('Service Is Exposed Externally', 'red')
+        except:
+            print colored("Service Not Exposed Exterally",'green')
 
-conn = ssh_connect(rem, 18861)
-#conn = rpyc.connect(host, 18861)
+        rem = SshMachine(host, user='ucb_268_measure', keyfile='~/.ssh/id_rsa')
+        conn = ssh_connect(rem, 18861)
+        conn.root.test()
+        a = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
+        b = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
+        c = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
 
-conn.root.test()
-a = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
-b = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
-c = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
-
-print a, b, c
+        if a and b and c:
+            print colored('Recieved Latencies','green'), a, b, c
+        else:
+            print ('Issue with latencies', 'red'), a, b, c
+    except Exception, e:
+        print colored('Could not connect', 'red')
