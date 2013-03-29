53a1233569796acfe24088617269e407d809eabc
ahirreddy <ahirreddy@gmail.com>
2013-03-29 01:14:02 -0700

    updated tkin-test

--- king/tking-test.py
+++ king/tking-test.py
@@ -1,4 +1,8 @@
 import rpyc
-c = rpyc.connect('pl2.planetlab.ics.tut.ac.jp', 18861)
-c.root.test()
-a = c.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
+conn = rpyc.connect('pl2.planetlab.ics.tut.ac.jp', 18861)
+conn.root.test()
+a = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
+b = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
+c = conn.root.get_latency('google.com','8.8.8.8','ns1.google.com','216.239.32.10')
+
+print a, b, c
