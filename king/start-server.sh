0c6559296fdc10688ce81a04fcbe4e5972e15925
ahirreddy <ahirreddy@gmail.com>
2013-03-29 14:43:09 -0700

    added echo

--- king/start-server.sh
+++ king/start-server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo nohup python /home/ucb_268_measure/CS268NetworkMeasurement/king/tking-server.py
+sudo nohup python /home/ucb_268_measure/CS268NetworkMeasurement/king/tking-server.py &
+echo 'Started Server'
