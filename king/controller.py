2455b2eea7faca58b5de16f97632d5bda150a1b8
ahirreddy <ahirreddy@gmail.com>
2013-04-12 14:56:59 -0700

    connect to planet lab nodes

--- king/controller.py
+++ king/controller.py
@@ -1,31 +1,33 @@
-import redis
+import redis, string
+from geoutils import distance
+from plumbum import SshMachine
+from rpyc.utils.factory import ssh_connect
 
-all_dns_connection = redis.ConnectionPool(host='localhost', port=6379, db=0)
-open_resolvers_connection = redis.ConnectionPool(host='localhost', port=6379, db=1)
-geoip_connection = redis.ConnectionPool(host='localhost', port=6379, db=2)
-
-all_dns = redis.Redis(connection_pool=all_dns_connection)
-open_resolvers = redis.Redis(connection_pool=open_resolvers_connection)
-geoip = redis.Redis(connection_pool=geoip_connection)
-pl_hosts = map(string.strip,open('pl-host-list').readlines())
-
+all_dns = redis.Redis(connection_pool=redis.ConnectionPool(host='localhost', port=6379, db=0))
+open_resolvers = redis.Redis(connection_pool=redis.ConnectionPool(host='localhost', port=6379, db=1))
+geoip = redis.Redis(connection_pool=redis.ConnectionPool(host='localhost', port=6379, db=2))
+pl_hosts = [line.split(' ')[0:4] for line in map(string.strip,open('pl-host-list-geo').readlines())]
 
 class PlanetLabNode(object):
-    def __init__(self, host):
+    def __init__(self, host, ip, lat, lon):
         self.host = host
+        self.ip = ip
+        self.lat = float(lat)
+        self.lon = float(lon)
         try:
-            self.conn = self.connectPL()
+            self.connectPL()
         except AssertionError, e:
             self.restartPL()
-            self.conn = self.connectPL()
+            self.connectPL()
 
     def connectPL(self):
-        rem = SshMachine(host, user='ucb_268_measure', keyfile='~/.ssh/id_rsa',
+        print 'Connecting to %s' % self.host
+        rem = SshMachine(self.host, user='ucb_268_measure', keyfile='~/.ssh/id_rsa',
                          ssh_opts=["-o StrictHostKeyChecking no",
                                    "-o UserKnownHostsFile=/dev/null"])
         conn = ssh_connect(rem, 18861)
         assert conn.root.test() == 1, "%s: RPC Failure" % host
-        return conn
+        self.conn = conn
 
     def restartPL(self):
         subprocess.call(["ssh", "-t", "-i", "~/.ssh/id_rsa", "-o StrictHostKeyChecking no",
@@ -34,8 +36,14 @@ class PlanetLabNode(object):
                          stdout=FNULL, stderr=subprocess.STDOUT)
 
     def get_latency(target1, ip1, target2, ip2):
-        return self.conn.root.exposed_get_latency(target1, ip1, target2, ip2)
+        return self.conn.root.get_latency(target1, ip1, target2, ip2)
 
     def get_k(target, ip):
-        return self.conn.exposed_get_k(target, ip)
+        return self.conn.get_k(target, ip)
 
+    def get_distance(lat, lon):
+        return distance((self.lat, self.lon), (lat, lon))
+
+pl_nodes = map(lambda args: PlanetLabNode(*args), pl_hosts)
+
+print pl_nodes
