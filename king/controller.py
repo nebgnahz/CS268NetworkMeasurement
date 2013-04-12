84824e8574eaa74255583f9f3a970c7a45c63da6
ahirreddy <ahirreddy@gmail.com>
2013-04-12 16:38:27 -0700

    better exception handling

--- king/controller.py
+++ king/controller.py
@@ -1,7 +1,7 @@
 import redis, string
-from utilities import distance, threaded_map
 from plumbum import SshMachine
 from rpyc.utils.factory import ssh_connect
+from utilities import distance, threaded_map
 
 all_dns = redis.Redis(connection_pool=redis.ConnectionPool(host='localhost', port=6379, db=0))
 open_resolvers = redis.Redis(connection_pool=redis.ConnectionPool(host='localhost', port=6379, db=1))
@@ -15,6 +15,9 @@ class PlanetLabNode(object):
         self.ip = ip
         self.lat = float(lat)
         self.lon = float(lon)
+        self.connect()
+
+    def connect(self):
         try:
             self.connectPL()
         except AssertionError, e:
@@ -22,19 +25,19 @@ class PlanetLabNode(object):
                 self.restartPL()
                 self.connectPL()
             except Exception, e:
-                self = None
+                self.connected = False
         except Exception, e:
-            self = None
-        print 'Done %s %i' % (host, id)
+            self.connected = False
 
     def connectPL(self):
-        print 'Connecting to %s %i' % (self.host, self.id)
+        #print 'Connecting to %s %i' % (self.host, self.id)
         rem = SshMachine(self.host, user='ucb_268_measure', keyfile='~/.ssh/id_rsa',
                          ssh_opts=["-o StrictHostKeyChecking no",
                                    "-o UserKnownHostsFile=/dev/null"])
         conn = ssh_connect(rem, 18861)
         assert conn.root.test() == 1, "%s: RPC Failure" % host
         self.conn = conn
+        self.connected = True
 
     def restartPL(self):
         subprocess.call(["ssh", "-t", "-i", "~/.ssh/id_rsa", "-o StrictHostKeyChecking no",
@@ -42,14 +45,52 @@ class PlanetLabNode(object):
                          "sudo tking-server stop; sudo tking-server start;"],
                          stdout=FNULL, stderr=subprocess.STDOUT)
 
-    def get_latency(target1, ip1, target2, ip2):
+    def handleConnExceptions(fn):
+        def wrapped(self, *args, **kwargs):
+            if not self.connected:
+                self.connect()
+
+            if self.connected:
+                try:
+                    return fn(self, *args, **kwargs)
+                except Exception, e:
+                    self.connected = False
+                    try:
+                        self.connect()
+                        return fn(self, *args, **kwargs)
+                    except Exception, e:
+                        self.connected = False
+                        return None
+            else:
+                return None
+        return wrapped
+
+    @handleConnExceptions
+    def get_latency(self, target1, ip1, target2, ip2):
         return self.conn.root.get_latency(target1, ip1, target2, ip2)
 
-    def get_k(target, ip):
+    @handleConnExceptions
+    def get_k(self, target, ip):
         return self.conn.get_k(target, ip)
 
-    def get_distance(lat, lon):
+    @handleConnExceptions
+    def get_distance(self, lat, lon):
         return distance((self.lat, self.lon), (lat, lon))
+
 
 pl_nodes = threaded_map(lambda x: PlanetLabNode(x[0], *x[1]), list(enumerate(pl_hosts)), timeout=10.0)
 
+def random_latency():
+    target1 = open_resolvers.randomkey()
+    target2 = geoip.randomkey()
+    while not geoip.exists(target1):
+        target1 = open_resolvers.randomkey()
+    ip1 = list(all_dns.smembers(target1))[0]#, list(geoip.smembers(target1))[1:]
+    ip2 = list(all_dns.smembers(target2))[0]#, list(geoip.smembers(target2))[1:]
+    return target1, ip1, target2, ip2
+
+results = threaded_map(lambda node: node.get_latency(*random_latency()), pl_nodes)
+
+print len(results)
+for r in results:
+    print r
