ac518f79e4b3ef18dd538bc07d0e7eb1d04ab131
ahirreddy <ahirreddy@gmail.com>
2013-04-14 14:51:06 -0700

    store multiple points in 1 db entry

--- king/controller.py
+++ king/controller.py
@@ -21,26 +21,26 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///latency.db', echo=False)
 Base = declarative_base(bind=engine)
 
-class DataPoint(Base):
-    __tablename__ = 'data'
-
-    id = Column(Integer, primary_key=True)
-    target1 = Column(PickleType)
-    target2 = Column(PickleType)
-    start = Column(PickleType)
-    end = Column(PickleType)
-    pings = Column(PickleType)
-    address = Column(PickleType)
-    test_point = Column(PickleType)
-
-    def __init__(self, target1, target2, start, end, pings, address, test_point):
-        self.target1 = target1
-        self.target2 = target2
+class DataPoint(object):
+    def __init__(self, start, end, pings, address, test_point):
         self.start = start
         self.end = end
         self.pings = pings
         self.address = address
         self.test_point = test_point
+
+class Query(Base):
+    __tablename__ = 'data'
+
+    id = Column(Integer, primary_key=True)
+    target1 = Column(PickleType)
+    target2 = Column(PickleType)
+    points = Column(PickleType)
+
+    def __init__(self, target1, target2, points)
+        self.target1 = target1
+        self.target2 = target2
+        self.points = points
 
 Base.metadata.create_all()
 
@@ -148,24 +148,22 @@ def doWork():
     while True:
         try:
             target1, target2, result_set = one_round()
+            points = []
             if result_set:
                 for host, info in filter(None,result_set):
                     if info:
                         try:
                             # TODO: Store None Responses As Well
                             (end_time, start_time, ping_times, address) = info
-                            point = DataPoint(target1, target2, start_time, end_time, ping_times, address, host)
-                            s.add(point)
+                            point = DataPoint(start_time, end_time, ping_times, address, host)
+                            points.append(point)
                         except Exception, e:
                             pass
+                data_set = Query(target1, target2, points)
+                s.add(data_set)
                 s.commit()
         except Exception, e:
-            print target1
-            print target2
-            print result_set
-            print '---------------------'
-            exit(1)
-            print e
+            pass
 
 pl_nodes = map(lambda args: PlanetLabNode(*args), pl_hosts)
 
