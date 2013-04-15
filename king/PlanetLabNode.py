1077aab0fad997e62e82aeedd1fb60e0f3c734f7
ahirreddy <ahirreddy@gmail.com>
2013-04-14 21:00:48 -0700

    my machine

--- king/PlanetLabNode.py
+++ king/PlanetLabNode.py
@@ -1,7 +1,17 @@
 import os, paramiko, rpyc, subprocess
+from plumbum.remote_machine import BaseRemoteMachine
 from plumbum.paramiko_machine import ParamikoMachine
 from rpyc.utils.factory import ssh_connect
 from utilities import distance, outputException
+
+class myMachine(ParamikoMachine):
+    def __init__(self, host, encoding='utf8'):
+        self.host = host
+        self._client = paramiko.SSHClient()
+        self._client.load_system_host_keys()
+        self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self._client.connect(host, username='ucb_268_measure', password=None)
+        BaseRemoteMachine.__init__(self, encoding)
 
 class PlanetLabNode(object):
     def __init__(self, host, ip, lat, lon):
@@ -24,9 +34,7 @@ class PlanetLabNode(object):
                 self.connected = False
 
     def connectPL(self):
-        rem = ParamikoMachine(self.host, user='ucb_268_measure',
-                             missing_host_policy=paramiko.AutoAddPolicy())
-
+        rem = myMachine(self.host)
         conn = ssh_connect(rem, 18861, config={'allow_pickle' : True})
         assert conn.root.test() == 1, "%s: RPC Failure" % self.host
         self.conn = conn
