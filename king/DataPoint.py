4c63ac45860b9e7a8a8d4429ff9d1b3667ff5740
ahirreddy <ahirreddy@gmail.com>
2013-04-15 18:16:56 -0700

    updated database type

--- king/DataPoint.py
+++ king/DataPoint.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, PickleType, Boolean
+from sqlalchemy import Column, Integer, PickleType, Boolean, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,13 +11,13 @@ class DataPoint(Base):
     __tablename__ = 'data'
 
     id = Column(Integer, primary_key=True)
-    target1 = Column(PickleType)
-    target2 = Column(PickleType)
+    target1 = Column(String)
+    target2 = Column(String)
     start = Column(PickleType)
     end = Column(PickleType)
     pings = Column(PickleType)
     address = Column(PickleType)
-    test_point = Column(PickleType)
+    test_point = Column(String)
     success = Column(Boolean)
 
     def __init__(self, target1, target2, start, end, pings, address, test_point, success):
