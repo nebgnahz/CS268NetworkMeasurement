8cb4ae8c27d78305f2685e8f80015730b0eef8ac
ahirreddy <ahirreddy@gmail.com>
2013-04-17 02:16:33 -0700

    added status script

--- king/CustomMachine.py
+++ king/CustomMachine.py
@@ -5,7 +5,10 @@ from plumbum.paramiko_machine import ParamikoMachine
 def monkey(self, host, encoding='utf8'):
     self.host = host
     self._client = paramiko.SSHClient()
-    self._client.load_system_host_keys()
+    try:
+        self._client.load_system_host_keys()
+    except:
+        pass
     self._client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     self._client.connect(host, username='ucb_268_measure', password=None)
     BaseRemoteMachine.__init__(self, encoding)
---
+++ king/tking-status.py
@@ -0,0 +1,73 @@
+import string, os, subprocess
+from datetime import datetime
+from threading import Thread, Lock
+from PlanetLabNode import PlanetLabNode
+from utilities import threaded_map
+from termcolor import colored
+from itertools import count
+
+pl_hosts = [line.split(' ')[0:4] for line in map(string.strip,open('pl-host-list-geo').readlines())]
+pl_nodes = map(lambda args: PlanetLabNode(*args), pl_hosts)
+thread_count = count()
+thread_count.next()
+#print_lock = Lock()
+
+def perNode(node):
+    for x in range(5):
+        try:
+            node.connectPL()
+            if node.conn.root.test() == 1:
+                #print_lock.acquire()
+                #print thread_count.next(), colored(node.host, 'green') 
+                #print_lock.release()
+                return node.host, True
+        except Exception as e:
+            ##print_lock.acquire()
+            ##print node.host, e
+            ##print_lock.release()
+            try:
+                FNULL = open(os.devnull, 'w')
+                subprocess.call(["ssh", "-t", "-i", "~/.ssh/id_rsa", "-o StrictHostKeyChecking no",
+                                 "-o UserKnownHostsFile=/dev/null", "ucb_268_measure@%s" % node.host,
+                                 "sudo tking-server stop; sudo tking-server start;"],
+                                 stdout=FNULL, stderr=FNULL)
+            except Exception, e:
+                pass
+            continue
+    #print_lock.acquire()
+    #print thread_count.next(), colored(node.host, 'red', timeout=)
+    #print_lock.release()
+    return node.host, False
+
+results = threaded_map(perNode, pl_nodes)
+
+import smtplib
+SERVER = "localhost"
+FROM = "ahir@internetlatencymeasurementstudy.org"
+TO = ["ahirreddy@gmail.com"] # must be a list
+SUBJECT = "Tking-Status %s" % str(datetime.now())
+
+TEXT = "Operational:\n"
+for host, success in results:
+    if success:
+        TEXT += "%s\n" % host
+TEXT = "\nNon-Operational:\n"
+for host, success in results:
+    if not success:
+        TEXT += "%s\n" % host
+
+# Prepare actual message
+
+message = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+# Send the mail
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.sendmail(FROM, TO, message)
+server.quit()
