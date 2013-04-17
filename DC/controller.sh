b98f939385c617a8d479d72b2bdad4212ae8baa6
nebgnahz <nebgnahz@gmail.com>
2013-04-16 22:02:56 -0700

    changed the cron job file and the .sh

--- DC/controller.sh
+++ DC/controller.sh
@@ -2,9 +2,9 @@
 LOG="/var/log/cron.log";
 FOLDER="/home/ucb_268_measure/CS268NetworkMeasurement/DC/"
 FILE="controller.py"
-echo "Start" >> $LOG;
+echo "Start_of_Schedule" >> $LOG;
 date >> $LOG;
 /usr/bin/python $FOLDER$FILE >> $LOG;
 date >> $LOG;
-echo "End" >> $LOG;
+echo "End_of_Schedule" >> $LOG;
 
