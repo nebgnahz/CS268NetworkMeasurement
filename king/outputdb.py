a38ebc2c0ab38c432aa5ab0bbd3e4e17705cb8ec
ahirreddy <ahirreddy@gmail.com>
2013-04-18 17:29:26 -0700

    output db working

--- king/outputdb.py
+++ king/outputdb.py
@@ -10,26 +10,26 @@ connection = MySQLdb.connect(host = "data.cnobwey0khau.us-west-2.rds.amazonaws.c
                              cursorclass = MySQLdb.cursors.SSCursor)
 
 cur = connection.cursor()
-cur.execute("SELECT count(*) from data;")
-print 'Entries: %i' % cur.fetchone()
+cur.execute("""SELECT
+	               SUM(IF(success, 1, 0)),
+	               SUM(IF(not success, 1, 0))
+               FROM data;""")
+num_success, num_fail = cur.fetchone()
+percent = float(num_success)/float(num_fail+num_success)
+print 'Success %i, Fail %i, Success Rate %s' % (num_success, num_fail, "{0:.0f}%".format(percent * 100)) 
 cur.close()
 
 cur = connection.cursor()
 cur.execute("SELECT * from data where success;")
 for r in cur:
     id, timestamp, name1, name2, target1, target2, start, end, pings, address, test_point, success = r
-    print 'Date of Measurement', timestamp
-    print name1, name2
-    print pickle.loads(target1)
-    print pickle.loads(target2)
-    print test_point
-    print pickle.loads(address)
-    print start
-    print end
-    print pickle.loads(pings)
+    print 'Date of Measurement', pickle.loads(timestamp)
+    print 'Target1:', name1, pickle.loads(target1)
+    print 'Target2:', name2, pickle.loads(target2)
+    print 'Test Point:', test_point
+    print 'Response Address', pickle.loads(address)
+    print 'Start:', pickle.loads(start)
+    print 'End:', pickle.loads(end)
+    print 'Pings:', pickle.loads(pings)
     print '---------------------------------'
-    break
 cur.close()
-
-#for r in s.query(DataPoint).filter(DataPoint.success == True,):
-
