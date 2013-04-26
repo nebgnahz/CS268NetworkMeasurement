862bb63808c62cfc88f30e9f26b2dc56d2d947cb
ahirreddy <ahirreddy@gmail.com>
2013-04-26 10:37:48 -0700

    added try block

--- king/filter.py
+++ king/filter.py
@@ -9,25 +9,29 @@ results = pickle.load(input_file)
 seen = defaultdict(list)
 
 for r in results:
-    id, timestamp, name1, name2, target1, target2, start, end, pings, address, test_point, success = r
-    timestamp =  pickle.loads(timestamp)
-    target1 = pickle.loads(target1)
-    target2 =  pickle.loads(target2)
-    address = pickle.loads(address)
-    start = pickle.loads(start)
-    end = pickle.loads(end)
-    pings = pickle.loads(pings)
+	try:
+	    id, timestamp, name1, name2, target1, target2, start, end, pings, address, test_point, success = r
+	    timestamp =  pickle.loads(timestamp)
+	    target1 = pickle.loads(target1)
+	    target2 =  pickle.loads(target2)
+	    address = pickle.loads(address)
+	    start = pickle.loads(start)
+	    end = pickle.loads(end)
+	    pings = pickle.loads(pings)
 
-    dist = distance(target1[2], target2[2])
+	    dist = distance(target1[2], target2[2])
 
-    latency = (end - start - min(pings)).total_seconds()
+	    latency = (end - start - min(pings)).total_seconds()
     
-    if latency > 0 \
-    and dist > 0 \
-    and target1[1] == address[0] \
-    and latency > (dist/3.0/100000) \
-    and latency < 5:
-        seen[(name1, name2)].append((dist, latency, test_point, timestamp))
+	    if latency > 0 \
+	    and dist > 0 \
+	    and target1[1] == address[0] \
+	    and latency > (dist/3.0/100000) \
+	    and latency < 5:
+	        seen[(name1, name2)].append((dist, latency, test_point, timestamp))
+	except:
+		print "Dropped Point"
+		continue
 
 for name1, name2 in seen.keys():
     if len(seen[(name1, name2)]) >= 2 and len(seen[name2, name1]) >= 2:
