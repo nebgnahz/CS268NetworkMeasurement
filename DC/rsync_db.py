f9ff30c8adce295f1fcc27b18d433c12d3417d60
ahirreddy <ahirreddy@gmail.com>
2013-04-25 17:51:23 -0700

    rsync of db working

--- DC/rsync_db.py
+++ DC/rsync_db.py
@@ -5,13 +5,13 @@ from utilities import threaded_map
 pl_hosts = [line.split(' ')[0] for line in map(string.strip,open('../king/pl-host-list-geo').readlines())]
 
 def get_db(host):
-    file_name = '/root/gQuery_%s.db' % host
+    file_name = 'gQuery_%s.db' % host
 
     cp = ["ssh", "-t", "-i", "~/.ssh/id_rsa", "-o StrictHostKeyChecking no",
           "-o UserKnownHostsFile=/dev/null", "ucb_268_measure@%s" % host,
-          "sudo cp %s ~/gQuery.db" % file_name]
+          "sudo cp /root/%s ~/" % file_name]
 
-    rsync = 'rsync -a -v -e ssh -z --progress ucb_268_measure@%s:gQuery.db %s.db' % (host, host)
+    rsync = 'rsync -a -v -e ssh -z --progress ucb_268_measure@%s:%s .' % (host, file_name)
 
     print cp
     print rsync
