6f718503e3d5e1c58ee67d8cf1090bc7323e181e
ahirreddy <ahirreddy@gmail.com>
2013-04-27 13:09:27 -0700

    script for pl geo kml

---
+++ king/geo/pl-geo.py
@@ -0,0 +1,16 @@
+import string
+
+pl_hosts = [line.split(' ')[2:4] for line in map(string.strip,open('../pl-host-list-geo').readlines())]
+
+print '<?xml version="1.0" encoding="UTF-8"?>'
+print '<kml xmlns="http://earth.google.com/kml/2.2">'
+print '    <Document>'
+print '       <Folder>'
+
+for log, lat in pl_hosts:
+    print "<Placemark><Point><coordinates>"
+    print log, ',', lat, ', 0'
+    print "</coordinates></Point></Placemark>"
+print '        </Folder>'
+print '    </Document>'
+print '</kml>'
