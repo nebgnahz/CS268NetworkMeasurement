fc92ac57dd01fc2eb2b4fe134d417c22564f2771
ahirreddy <ahirreddy@gmail.com>
2013-04-28 16:53:47 -0700

    updated rsync and buffer

--- DC/process_db.py
+++ DC/process_db.py
@@ -1,5 +1,6 @@
 import os, operator
 
+from StringIO import StringIO
 from multiprocessing import Pool, Value
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -19,6 +20,7 @@ def extract(db_name):
         results = []
         engine = create_engine('sqlite:///' + db_name, echo=False)
         Base = declarative_base(bind=engine)
+        output_file = open(db_name+'.csv', 'w')
 
         class Query(Base):
             __tablename__ = 'data'
@@ -44,14 +46,30 @@ def extract(db_name):
         Session = sessionmaker(bind=engine)
         s = Session()
         for r in s.query(Query).yield_per(1000):
-            process_point(r)
+            out = process(r)
+            print >> output_file, out
+        output_file.close()
         print db_name, 'Done'
     except Exception, e:
         print db_name, e
 
-def process_point(r):
-    # Insert processing code here
-    pass
+def process(r):
+    output = StringIO()
+    print >> output, r.query,
+    print >> output, r.index,
+    print >> output, r.return_ip,
+    print >> output, r.queryTime.total_seconds(),
+    if not r.googleTime:
+        print >> output, '-1',
+    else:
+        print >> output, r.googleTime.total_seconds(),
+
+    if not r.pingTime:
+        print >> output, '-1',
+    else:
+        print >> output, min(r.pingTime).total_seconds(),
+
+    return output.getvalue()
 
 p = Pool(3)
 p.map(extract, files)
--- DC/rsync_db.py
+++ DC/rsync_db.py
@@ -5,18 +5,17 @@ pl_hosts = [line.split(' ')[0] for line in map(string.strip,open('../king/pl-hos
 
 def get_db(host):
     file_name = 'gQuery_%s.db' % host
+    log_file = open(file_name + '.log', 'w')
 
-    cp = ["ssh", "-t", "-i", "~/.ssh/id_rsa", "-o StrictHostKeyChecking no",
+    cp = ["ssh", "-t -t", "-i", "~/.ssh/id_rsa", "-o StrictHostKeyChecking no",
           "-o UserKnownHostsFile=/dev/null", "ucb_268_measure@%s" % host,
           "sudo cp /root/%s ~/" % file_name]
 
     rsync = 'rsync -a -v -e ssh -z --progress --partial ucb_268_measure@%s:%s .' % (host, file_name)
 
-    print cp
-    print rsync
-
-    subprocess.call(cp)
-    subprocess.call(rsync, shell=True)
+    print file_name,
+    print subprocess.call(cp, stdin=subprocess.PIPE, stdout=log_file, stderr=log_file),
+    print subprocess.call(rsync, shell=True, stdin=subprocess.PIPE, stdout=log_file, stderr=log_file)
 
 p = Pool(10)
 p.map(get_db, pl_hosts)
